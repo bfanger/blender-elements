81c046bebdce52a55db2d0956089367df8e5291f
Bob Fanger <bfanger@gmail.com>
2016-10-23 17:16:07 +0200

    Implemented <b-group> (styling only)

--- src/elements/ButtonElement.ts
+++ src/elements/ButtonElement.ts
@@ -3,14 +3,14 @@ const style = require("./ButtonElement.scss")
 
 const dom = render(`
 <button class="${style.button}">
-    <img class="${style.button__icon}" ref="icon"/>
+    <img class="${style.button__icon}" ref="icon">
     <span class="${style.button__label}" ref="label"></span>
 </button>
 `)
 
 class ButtonElement extends HTMLElement {
-    public icon:string
     public label:string
+    public icon:string
 
     static get observedAttributes() {
         return ['label', 'icon']
@@ -23,21 +23,21 @@ class ButtonElement extends HTMLElement {
             element.setAttribute('label', label);
             element.innerHTML = ''
         }
-        Object.defineProperty(element, 'icon', {
-            get() {
-                return element.getAttribute('icon')
-            },
-            set(value) {
-                element.setAttribute('icon', value)
-                element.render()
-            }
-        })
         Object.defineProperty(element, 'label', {
             get() {
                 return element.getAttribute('label')
             },
             set(value) {
                 element.setAttribute('label', value)
+                element.render()
+            }
+        })
+        Object.defineProperty(element, 'icon', {
+            get() {
+                return element.getAttribute('icon')
+            },
+            set(value) {
+                element.setAttribute('icon', value)
                 element.render()
             }
         })
@@ -54,20 +54,20 @@ class ButtonElement extends HTMLElement {
         this.children.item(0).remove();
     }
     private render() {
-        const icon = this.querySelector('[ref="icon"]') as HTMLImageElement
-        if (!icon) {
+        const label = this.querySelector('[ref="label"]') as HTMLSpanElement
+        if (!label) {
             return
         }
-        if (this.hasAttribute('icon')) {
-            icon.src = this.getAttribute('icon') as string 
-        } else {
-            icon.removeAttribute('src') 
-        }
-        const label = this.querySelector('[ref="label"]') as HTMLSpanElement
         if (this.hasAttribute('label')) {
             label.textContent = this.getAttribute('label') as string 
         } else {
             label.textContent = '' 
+        }
+        const icon = this.querySelector('[ref="icon"]') as HTMLImageElement
+        if (this.hasAttribute('icon')) {
+            icon.src = this.getAttribute('icon') as string 
+        } else {
+            icon.removeAttribute('src') 
         }
     }
 }
